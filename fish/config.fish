a8896e8aa3935af6887a6839db2b92aac5452659
hakanbiris <hakanbiris@icloud.com>
2024-07-23 21:33:18 +0300

    ek

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 set fish_greeting
 set -x EDITOR hx
 set -x TERMINAL alacritty
-set -x PATH "$HOME/.cargo/bin" "$HOME/.local/bin" "/opt/homebrew/bin" "/opt/homebrew/sbin" "/usr/local/bin" "$PATH"
+set -x PATH "$HOME/.cargo/bin" "$HOME/.local/bin" /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin "$PATH"
+set -x RUSTFLAGS -Awarnings
 function gecmis
     builtin history --show-time='%F %T '
 end
@@ -25,11 +26,4 @@ function ll --wraps=ls --wraps=exa --description 'List contents of directory usi
     exa --tree --level=2 -a --long --header --accessed --git $argv
 end
 
-alias ls='exa --long --color=always --group-directories-first --icons'
-alias la='exa -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons' # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="exa -a | egrep '^\.'" # show only dotfiles
 alias cat='bat --style header --style rule --style snip --style changes --style header'
-alias zll="zellij"
-alias rustz="zellij action new-tab -l /Users/hakanbiris/.config/zellij/layouts/rust.kdl"
---
+++ fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps=ls --wraps=exa --wraps=eza_git --description 'alias ll eza_git'
+  eza_git $argv
+        
+end
